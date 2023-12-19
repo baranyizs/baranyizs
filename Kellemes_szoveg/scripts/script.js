f56ac9e0de15aea4327c8c8c7ee7f96d29963337
baranyizs <baranyizs@gmail.com>
2023-12-19 01:43:47 +0100

    lazy_load to page by page, adding text

--- Kellemes_szoveg/scripts/script.js
+++ Kellemes_szoveg/scripts/script.js
@@ -1,34 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const lazyImages = document.querySelectorAll('.lazy-load');
-  
-    const lazyLoad = function() {
-      lazyImages.forEach(img => {
-        const rect = img.getBoundingClientRect();
-        if (rect.top >= 0 && rect.bottom <= window.innerHeight && !img.src) {
-          img.src = img.dataset.src;
-          img.onload = function() {
-            img.style.opacity = 1;
-          };
-        }
-      });
-    };
-  
-    const toggleImage = function(event) {
-      const img = event.target;
-      if (img.style.opacity === '1') {
-        img.style.opacity = 0;
-      } else {
-        img.style.opacity = 1;
-      }
-    };
-  
-    window.addEventListener('scroll', lazyLoad);
-    lazyLoad();
-  
-    lazyImages.forEach(img => {
-      img.addEventListener('click', toggleImage);
-    });
-  });
+const content = document.querySelectorAll('main');
 
-  // Az összes kép azonosítása
-const images = document.querySelectorAll('.image-gallery img');
+let contentIndex = 0;
+
+content.forEach(x => {
+  x.addEventListener('click', e => {
+    content[contentIndex].classList.remove('show-content');
+    if (contentIndex < content.length - 1) {
+      contentIndex++;
+    } else {
+      contentIndex = 0;
+    }
+    content[contentIndex].classList.add('show-content');
+  });
+});
+
+content[contentIndex].classList.add('show-content');
